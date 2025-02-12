54e93453cec68e6a37a514323ae68c87ea5b2362
himanshunegi1419 <himanshunegi1419@gmail.com>
2025-02-12 17:40:13 +0530

    add vercel url

--- index.js
+++ index.js
@@ -3,20 +3,25 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
-require('dotenv').config();
+require("dotenv").config();
 
-
-app.use(cors({
-    origin: ["http://localhost:5173", "https://bookmart-l3eiv590i-himanshu-negis-projects-06fc20b1.vercel.app"],
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://bookmart-l3eiv590i-himanshu-negis-projects-06fc20b1.vercel.app",
+      "https://bookmart-7dhybh6qa-himanshu-negis-projects-06fc20b1.vercel.app",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-}));
+    credentials: true,
+  })
+);
 
 // ✅ Middleware
 app.use(express.json());
 
 // ✅ Routes
-const bookRoutes = require('./src/books/book.route');
+const bookRoutes = require("./src/books/book.route");
 const orderRoutes = require("./src/orders/order.route");
 const userRoutes = require("./src/users/user.route");
 const adminRoutes = require("./src/stats/admin.stats");
@@ -26,19 +31,18 @@ app.use("/api/orders", orderRoutes);
 app.use("/api/auth", userRoutes);
 app.use("/api/admin", adminRoutes);
 
-
 app.get("/", (req, res) => {
-    res.send("Book Store Server is running!");
+  res.send("Book Store Server is running!");
 });
 
 // ✅ Database Connection
 async function main() {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("Mongodb connected successfully!");
+  await mongoose.connect(process.env.MONGO_URI);
+  console.log("Mongodb connected successfully!");
 }
 
-main().catch(err => console.log(err));
+main().catch((err) => console.log(err));
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
